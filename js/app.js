4b9387873987a0724f18b2da83fe2360429068c2
thefrostyausty <austinrobinson0000@gmail.com>
2022-02-18 02:17:58 -0500

    Attempting to fix this

--- js/app.js
+++ js/app.js
@@ -18,36 +18,30 @@ const box8 = document.getElementById('8')
 // I want to make a function that contains a condition that will display
 // a message after a 'click' to say "Player One or Two it's your turn"
 // now I will make a function to say when it is the players turn
+const winnerMessage = document.querySelector('#winnerMessage')
 const playerMessage = document.querySelector('#playerMessage');
-let currentPlayer = 'X'
+
 let xMoves = []
 let oMoves = []
+let currentPlayer = 'X'
 
 const displayCurrentPlayer = () => {
     playerMessage.innerText = `It's ${currentPlayer}'s turn`
 }
-
 displayCurrentPlayer()
-
-
 
 // I want to show who won a match.
 // this is the constant that holds the id HTML element
 // here i utilized my vairable message which is connected to the ID end results to display when a winner has won
-const winnerMessage = document.querySelector('#winnerMessage')
-
 const displayWinner = (winner) => {
     winnerMessage.innerText = `Player ${winner} won!`
 }
-
-
-
 // When a player clicks on a position on the Tic Tac Toe Board,
 const symbols = ['X', 'O']
 const boardPositions = document.querySelectorAll('.row div')
 
 const takeTurn = (event) => {
-
+    
     // 1. The current player's symbol should be filled in in that position IF that position isn't already taken.
     // the variable targetValue represents the element that was clicked on's value
     
@@ -56,62 +50,60 @@ const takeTurn = (event) => {
     let positionIsTaken = symbols.includes(positionValue)  // Googled: how to check if value is in an array and received assistance from friend/tutor
     let gameOver = false;
     
-    if (!positionIsTaken && !gameOver) {
+    if (!positionIsTaken || !gameOver) {
         positionDiv.innerText = currentPlayer
         console.log(positionDiv.id)
         // 2. A winner should be checked for AND declared if a winner exists
         // I have an array with the winning combinations
         // let winningCombinations = [[0, 1, 2], [1, 4, 7], [2, 5, 8], [0, 3, 6], [2, 4, 6], [3, 4, 5], [6, 7, 8], [0, 4, 8]]
         // if (currentPlayer == 'X') {
-        // } 
-
-        if (box0.innerText == box1.innerText && box1.innerText == box2.innerText||
-            box1.innerText == box4.innerText && box1.innerText == box7.innerText||
-            box2.innerText == box5.innerText && box2.innerText == box8.innerText||
-            box0.innerText == box3.innerText && box0.innerText == box6.innerText||
-            box2.innerText == box4.innerText && box2.innerText == box6.innerText||
-            box3.innerText == box4.innerText && box3.innerText == box5.innerText||
-            box6.innerText == box7.innerText && box6.innerText == box8.innerText||
-            box0.innerText == box4.innerText && box0.innerText == box2.innerText
-            ){
-                displayWinner(currentPlayer)
-                 gameOver = true;
-                 console.log(gameOver)
-        }           
+            // } 
+                if (box0.innerText == box1.innerText && box1.innerText == box2.innerText||
+                    box1.innerText == box4.innerText && box1.innerText == box7.innerText||
+                    box2.innerText == box5.innerText && box2.innerText == box8.innerText||
+                    box0.innerText == box3.innerText && box0.innerText == box6.innerText||
+                    box2.innerText == box4.innerText && box2.innerText == box6.innerText||
+                    box3.innerText == box4.innerText && box3.innerText == box5.innerText||
+                    box6.innerText == box7.innerText && box6.innerText == box8.innerText||
+                    box0.innerText == box4.innerText && box0.innerText == box2.innerTex){
+                
+            }   
+                    // displayWinner(currentPlayer)                 
+                }           
+                
+                // console.log(box8.innerText)
+                // 3. If there's no winner, the next player's turn should start
+                currentPlayer = currentPlayer === 'X' ? 'O': 'X'
+                displayCurrentPlayer()
+            }
+                
+                
+            
+            // takeTurn()
         
-        // console.log(box8.innerText)
-
-
-
-        // 3. If there's no winner, the next player's turn should start
-        currentPlayer = currentPlayer === 'X' ? 'O': 'X'
-        displayCurrentPlayer()
-    } else{
-        console.log(gameOver)
-    }
-}
-
-
-for (let i = 0; i < boardPositions.length; i++ ) {
-    // Assign the boardPosition at the current index to it's own value
-    // i put boardPostion to equte to the index/divs
-    let boardPosition = boardPositions[i]
-    boardPosition.addEventListener('click', takeTurn)
-}
-
-
-
-// this should be able to clear my board and click when a player has a turn
-const resetButton = document.querySelector('#reset')
-
-const resetBoard = () => {
-    for (let i = 0; i < boardPositions.length; i++ ) {
-        let boardPosition = boardPositions[i]
-        boardPosition.innerText = i
-    }
-
-    currentPlayer = 'X'
-    displayCurrentPlayer()
-}
-
-resetButton.addEventListener('click', resetBoard)
+        displayWinner()
+        
+        for (let i = 0; i < boardPositions.length; i++ ) {
+        // Assign the boardPosition at the current index to it's own value
+            // i put boardPostion to equte to the index/divs
+            let boardPosition = boardPositions[i]
+            boardPosition.addEventListener('click', takeTurn)
+        }
+        
+        
+        
+        // this should be able to clear my board and click when a player has a turn
+        const resetButton = document.querySelector('#reset')
+        
+        const resetBoard = () => {
+            for (let i = 0; i < boardPositions.length; i++ ) {
+                let boardPosition = boardPositions[i]
+                boardPosition.innerText = i
+                // displayWinner()
+            }
+            
+            currentPlayer = 'X'
+            displayCurrentPlayer()
+        }
+        
+        resetButton.addEventListener('click', resetBoard)
